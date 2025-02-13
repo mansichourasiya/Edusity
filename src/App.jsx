e5a97fca9906e892c857095cb3026c824a0fd300
Mansi Chourasiya <mansichourasia295gmail.com@Mansis-MacBook-Air.local>
2025-02-13 12:32:37 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import Programs from './components/programs/programs.jsx'; 
+
 
 
 
